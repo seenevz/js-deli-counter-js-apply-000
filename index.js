11386e8e71fd123209b9c70e9614ec4beab085b1
Sergio Neves <serneves@hotmail.com>
2018-11-18 00:58:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = []
 
 function takeANumber (katzDeliLine, name) {
-
-  return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
